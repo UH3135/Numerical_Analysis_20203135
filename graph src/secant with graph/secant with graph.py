9107ecdeea8d5618dc6737295a7b0e426b48cccd
UH3135 <66198389+UH3135@users.noreply.github.com>
2021-05-23 16:50:23 +0900

    Update secant with graph.py
    할선법 그래프 소스코드 작성

--- graph src/secant with graph/secant with graph.py
+++ graph src/secant with graph/secant with graph.py
@@ -1,8 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+xran = np.linspace(2.5, 4.5, 100)
+
+f = lambda x: np.sin(10 * x) + np.cos(3 * x)
+
 def secant(num1, num2, func):
-    ran = np.linspace(30, 40, 100)
     es = 10 ** -5
     cnt = 0
 
@@ -10,12 +13,15 @@ def secant(num1, num2, func):
         fnum1 = func(num1)
         fnum2 = func(num2)
 
+        f = lambda x: (fnum2 - fnum1) / (num2 - num1) * (x - num1) + fnum1
+        plt.plot(xran, f(xran), 'b:')
+
         temp = num1 - fnum1 * (num2 - num1) / (fnum2 - fnum1)
         ea = np.abs((num2 - num1) / num2)
 
-        plt.scatter(num1, fnum1, c='b')
+        plt.scatter(num1, fnum1, c='k')
         cnt += 1
-        plt.text(num1, fnum1 + 0.0001, "f{}".format(cnt), fontsize=8)
+        plt.text(num1, fnum1 + 0.1, "f{}".format(cnt), fontsize=10)
 
 
 
@@ -28,13 +34,12 @@ def secant(num1, num2, func):
 
 
 if __name__ == '__main__':
-    xran = np.linspace(30, 40, 100)
+    plt.plot(xran, f(xran))
+    plt.axhline(0, 0, 1)
+    plt.xlim(2.9, 4)
+    plt.ylim(-2.1, 1.9)
 
-    func = lambda x: (2 * 9.81 / 1000) - 1.4 * 10**-5 * x**1.5 - 1.15 * 10**-5 * x**2
-    plt.plot(xran, func(xran))
-
-    num1, num2, ea = secant(30, 30.5, func)
+    num1, num2, ea = secant(3, 3.5, f)
     print(f"최종 근은 {num2}, 최종 상대오차는 {ea}")
 
-    plt.axvline(num2, 0, 0.27, c='gray', linestyle='--')
     plt.show()
