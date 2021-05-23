841985eafa7c739cda0f2adbe9d2adf81f435223
UH3135 <rabbitdy0101@kookmin.ac.kr>
2021-05-23 09:17:13 +0900

    Update newton-raphson.py
    뉴튼 랩슨법의 소스코드를 완성된 것으로 변경

--- basic src/Newton-Raphson Method/newton-raphson.py
+++ basic src/Newton-Raphson Method/newton-raphson.py
@@ -3,34 +3,30 @@ import numpy as np
 
 def newtonRaphson(start, func:sp.Add, dfunc:sp.Add):
     es = 10 ** -5
+    x = sp.symbols('x')
 
     cnt = 0
 
-    x = start
+    var = start
 
     while True:
-        oldX = x
-        a = func.subs({x: oldX})
-        b = dfunc.subs({x: oldX})
+        oldVar = var
+        var = var - func.subs({x: var}) / dfunc.subs({x: var})
 
-        x = x - (a / b)
-
-        ea = np.abs((x - oldX) / x)
+        ea = np.abs((var - oldVar) / var)
         cnt += 1
-
-        print(f"x: {oldX}, func(x): {x}, ea: {ea}, a: {a}, b: {b}")
 
         if ea < es:
             break
 
-    return x, ea, cnt
+    return var, ea, cnt
 
 
 if __name__ == '__main__':
     x = sp.symbols('x')
-    f = x**2  - 3*x + np.e**x - 2
-    fprime = sp.Derivative(f, x).doit()
-    start = 0
-    x, ea, cnt = newtonRaphson(start, f, fprime)
+    func = x ** 2 - 3 * x + np.e**x - 2
+    dfunc = sp.Derivative(func, x).doit()
 
-    print(f"근은 {x}, 상대오차는 {ea}, 총 {cnt}번 반복했습니다.")
+    var, ea, cnt = newtonRaphson(0, func, dfunc)
+
+    print(f"근의 값은 {var}이고, 상대 오차 값은 {ea}이고, 총 {cnt}번 반복했다.")
